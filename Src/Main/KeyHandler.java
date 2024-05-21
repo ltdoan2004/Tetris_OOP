1875b5c0f8a3c306c0c5e5d669214e62e39f44a2
Lê Thiên Doanh <doa_ai@MacBook-Pro-cua-Le-2.local>
2024-05-21 21:25:10 +0700

    fix

--- Src/Main/KeyHandler.java
+++ Src/Main/KeyHandler.java
@@ -16,8 +16,8 @@ public class KeyHandler implements KeyListener {
         int code = e.getKeyCode();
         if(code == KeyEvent.VK_W || code == KeyEvent.VK_UP) {upPressed = true;}
         if(code == KeyEvent.VK_A || code == KeyEvent.VK_LEFT) {leftPressed = true;}
-//        if(code == KeyEvent.VK_S || code == KeyEvent.VK_DOWN) {downPressed = true;}
-//        if(code == KeyEvent.VK_D || code == KeyEvent.VK_RIGHT) {rightPressed = true;}
+        if(code == KeyEvent.VK_S || code == KeyEvent.VK_DOWN) {downPressed = true;}
+        if(code == KeyEvent.VK_D || code == KeyEvent.VK_RIGHT) {rightPressed = true;}
         if(code == KeyEvent.VK_SPACE) {dropPressed = true;}
         if(code == KeyEvent.VK_ESCAPE) {
             if (pausePressed) {
