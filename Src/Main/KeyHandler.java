bd2bbaf13c209fa6e68243e64598d0c293beb343
Lê Thiên Doanh <doa_ai@MacBook-Pro-cua-Le-2.local>
2024-05-21 11:48:29 +0700

    update keyhandler

---
+++ Src/Main/KeyHandler.java
@@ -0,0 +1,39 @@
+package Main;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class KeyHandler implements KeyListener {
+    public static boolean upPressed, downPressed, leftPressed, rightPressed, dropPressed, pausePressed;
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        int code = e.getKeyCode();
+        if(code == KeyEvent.VK_W || code == KeyEvent.VK_UP) {upPressed = true;}
+        if(code == KeyEvent.VK_A || code == KeyEvent.VK_LEFT) {leftPressed = true;}
+        if(code == KeyEvent.VK_S || code == KeyEvent.VK_DOWN) {downPressed = true;}
+        if(code == KeyEvent.VK_D || code == KeyEvent.VK_RIGHT) {rightPressed = true;}
+        if(code == KeyEvent.VK_SPACE) {dropPressed = true;}
+        if(code == KeyEvent.VK_ESCAPE) {
+            if (pausePressed) {
+                pausePressed = false;
+                GamePanel.music.play(0, true);
+                GamePanel.music.loop();
+            }
+            else {
+                pausePressed = true;
+                GamePanel.music.stop();
+            }
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
+}
