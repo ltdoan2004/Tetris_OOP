fc0fd9adb75987223bc304187123428e8b5623e7
Dew <ITCSIU22017@student.hcmiu.edu.vn>
2024-05-23 15:02:59 +0700

    Modified a little bit

--- Src/Main/GamePanel.java
+++ Src/Main/GamePanel.java
@@ -1,4 +1,4 @@
-package Main;
+package Src.Main;
 
 import javax.swing.*;
 import java.awt.*;
--- Src/Main/KeyHandler.java
+++ Src/Main/KeyHandler.java
@@ -1,4 +1,4 @@
-package Main;
+package Src.Main;
 
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
