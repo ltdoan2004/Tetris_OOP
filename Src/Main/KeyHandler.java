09e96019b349bd74c2b2c864df45023ac6257220
ltdoan2004 <doanh25032004@gmail.com>
2024-05-29 23:41:12 +0700

    fix Keyhandler

--- Src/Main/KeyHandler.java
+++ Src/Main/KeyHandler.java
@@ -11,20 +11,20 @@ public class KeyHandler implements KeyListener {
 
     }
 
-    @Override
-    public void keyPressed(KeyEvent e) {
-        int code = e.getKeyCode();
-        if(code == KeyEvent.VK_W || code == KeyEvent.VK_UP) {upPressed = true;}
-        if(code == KeyEvent.VK_A || code == KeyEvent.VK_LEFT) {leftPressed = true;}
-        if(code == KeyEvent.VK_S || code == KeyEvent.VK_DOWN) {downPressed = true;}
-        if(code == KeyEvent.VK_D || code == KeyEvent.VK_RIGHT) {rightPressed = true;}
-        if(code == KeyEvent.VK_SPACE) {dropPressed = true;}
-        if(code == KeyEvent.VK_ESCAPE) {
-            if (pausePressed) {
-                pausePressed = false;
-                GamePanel.music.play(0, true);
-                GamePanel.music.loop();
-            }
+//    @Override
+//    public void keyPressed(KeyEvent e) {
+//        int code = e.getKeyCode();
+//        if(code == KeyEvent.VK_W || code == KeyEvent.VK_UP) {upPressed = true;}
+//        if(code == KeyEvent.VK_A || code == KeyEvent.VK_LEFT) {leftPressed = true;}
+//        if(code == KeyEvent.VK_S || code == KeyEvent.VK_DOWN) {downPressed = true;}
+//        if(code == KeyEvent.VK_D || code == KeyEvent.VK_RIGHT) {rightPressed = true;}
+//        if(code == KeyEvent.VK_SPACE) {dropPressed = true;}
+//        if(code == KeyEvent.VK_ESCAPE) {
+//            if (pausePressed) {
+//                pausePressed = false;
+//                GamePanel.music.play(0, true);
+//                GamePanel.music.loop();
+//            }
             else {
                 pausePressed = true;
                 GamePanel.music.stop();
